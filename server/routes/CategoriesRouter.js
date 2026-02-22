b3d405f69d69453a3b3f961cb5bea1006c9750d4
Meg7373 <meaghannlw@gmail.com>
2026-02-22 12:21:03 -0500

    update categories

--- server/routes/CategoriesRouter.js
+++ server/routes/CategoriesRouter.js
@@ -1,8 +1,25 @@
 import express from "express";
+import db from "../dataDBConnections.js";
+
 const router = express.Router();
 
-router.get("/",(req,res)=>{
- res.send("working");
+// GET ALL CATEGORIES FROM MYSQL
+router.get("/", async (req,res)=>{
+
+try{
+
+const [rows] = await db.query("SELECT * FROM categories");
+
+res.json(rows);
+
+}
+catch(err){
+
+console.log(err);
+res.status(500).json({error:"db error"});
+
+}
+
 });
 
 export default router;
