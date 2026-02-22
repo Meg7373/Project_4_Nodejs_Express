2bda5876716e0a212cbaf7985fe9fbb3b845fb17
Meg7373 <meaghannlw@gmail.com>
2026-02-22 12:21:39 -0500

    update questions

--- server/routes/QuestionsRouter.js
+++ server/routes/QuestionsRouter.js
@@ -1,8 +1,46 @@
 import express from "express";
+import db from "../dataDBConnections.js";
+
 const router = express.Router();
 
-router.get("/",(req,res)=>{
- res.send("working");
+// GET QUESTIONS
+router.get("/", async(req,res)=>{
+
+try{
+
+const [rows] = await db.query("SELECT * FROM questions");
+
+res.json(rows);
+
+}
+catch(err){
+res.status(500).json(err);
+}
+
+});
+
+// ADD QUESTION
+router.post("/", async(req,res)=>{
+
+const {title,body} = req.body;
+
+try{
+
+await db.query(
+"INSERT INTO questions(title,body) VALUES (?,?)",
+[title,body]
+);
+
+res.json({success:true});
+
+}
+catch(err){
+
+console.log(err);
+res.status(500).json(err);
+
+}
+
 });
 
 export default router;
